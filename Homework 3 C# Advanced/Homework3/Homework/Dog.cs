1c5cac5a80765744825e1a11b3c8171214606a9f
simon-k1993 <kozoloski_simon@yahoo.com>
2023-05-03 02:08:26 +0200

    Added Homework3

---
+++ Homework 3 C# Advanced/Homework3/Homework/Dog.cs
@@ -0,0 +1,10 @@
+﻿internal class Dog
+{
+    public Dog()
+    {
+    }
+
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Color { get; set; }
+}
---
+++ Homework 3 C# Advanced/Homework3/Homework3/Program.cs
@@ -0,0 +1,69 @@
+﻿using Domain;
+using Newtonsoft.Json;
+
+string folderPath = @"..\..\..\DataBase";
+string filePath = @"..\..\..\DataBase\jsonFile.json";
+
+if (!Directory.Exists(folderPath))
+{
+    Directory.CreateDirectory(folderPath);
+}
+
+try { 
+if (!File.Exists(filePath))
+{
+    File.Create(filePath).Close();
+}
+
+List<Dog> dogsList = new List<Dog>();
+
+string jsonFileContent = File.ReadAllText(filePath);
+if (!string.IsNullOrWhiteSpace(jsonFileContent))
+{
+    dogsList = JsonConvert.DeserializeObject<List<Dog>>(jsonFileContent);
+}
+
+Console.WriteLine("Enter the dog's name:\n");
+string dogName = Console.ReadLine();
+Console.WriteLine();
+
+Console.WriteLine("Enter the dog's age:\n");
+int dogAge = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+Console.WriteLine("Enter the dog's color:\n");
+string dogColor = Console.ReadLine();
+Console.WriteLine();
+
+Dog newDog = new Dog()
+{
+    Name = dogName,
+    Age = dogAge,
+    Color = dogColor
+};
+
+dogsList.Add(newDog);
+
+string jsonString = JsonConvert.SerializeObject(dogsList);
+File.WriteAllText(filePath, jsonString);
+
+PrintDogs(dogsList);
+}
+catch (Exception ex)
+{   
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("ERROR: " + ex.Message);
+    Console.ResetColor();
+}
+
+static void PrintDogs(List<Dog> dogsList)
+{
+    foreach (Dog dog in dogsList)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"{dog.Name} the dog is {dog.Age} years old and is {dog.Color}!\n");
+        Console.ResetColor();
+    }
+}
+
+
